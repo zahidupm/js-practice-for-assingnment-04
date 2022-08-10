14bcd27b409d703f807bce747d6256caa2fdb76f
zahidupm <zahidhasanlimon111@gmail.com>
2022-08-10 12:58:31 +0600

    element ke notun array te push kora

--- function-array.js
+++ function-array.js
@@ -1,13 +1,32 @@
-function getSum(numbers) {
-    let sum = 0;
+// function getSum(numbers) {
+//     let sum = 0;
+//     for(let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         sum = sum + element;
+//         console.log(element, sum);
+//     }
+//     return sum;
+// }
+
+// const myNumbers = [12, 56, 65, 45, 78, 43, 91];
+// getSum(myNumbers);
+
+
+function oddNumberOfAnArray(numbers) {
+    const oddNumbers = [];
     for(let i = 0; i < numbers.length; i++) {
         const index = i;
         const element = numbers[index];
-        sum = sum + element;
-        console.log(element, sum);
+        if(element % 2 === 0) {
+            // console.log(element);
+            oddNumbers.push(element); // মান নতুন array তে দেয়ার জন্য
+
+        }
     }
-    return sum;
+    return oddNumbers;
 }
 
-const myNumbers = [12, 56, 65, 45, 78, 43, 91];
-getSum(myNumbers);
+const myNumber = [12, 56, 65, 45, 78, 43, 91];
+const oddNumbers = oddNumberOfAnArray(myNumber);
+console.log(oddNumbers);
